4a6f6cd04314675f252db1a74a58ed84915d9077
niyo-sean <niyibizibonaventure33@gmail.com>
2023-02-17 07:23:14 -0800

    12.task ad last

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints all possible combinations of two two-digit numbers
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	putchar('0' + 0);
+	putchar('0' + 0);
+	putchar(' ');
+	putchar('0' + 0);
+	putchar('0' + 1);
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
