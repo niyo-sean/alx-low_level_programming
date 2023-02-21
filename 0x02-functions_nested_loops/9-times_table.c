732ae52ee2e325084dfd3367f56485e5c7a63a1a
niyo-sean <niyibizibonaventure33@gmail.com>
2023-02-21 07:35:59 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,12 +22,13 @@ void time_teble(void)
 	_putchar(d + '0');
 	_putchar(u + '0');
 	}
-	else if (y != 0);
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
+	else
+		if (y != 0);
+	        {
+		_putchar(44);
+		_putchar(32);
+		_putchar(32);
+		}
 	_putchar(z + '0');
 	}
 	}
