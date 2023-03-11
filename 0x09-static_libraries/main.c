225300a27cbcfd175bbb2dbb8c5cc6f3cc084120
niyo-sean <niyibizibonaventure33@gmail.com>
2023-03-10 16:10:37 -0800

    main c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,13 @@
 #include "main.h"
-
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n");
+	_puts("\t- Kevin Mitnick");
+	return (0);
 }
-/*owned by NIYIBIZI Bonaventure */
+/* owned by NIYIBIZI Bonaventure */
