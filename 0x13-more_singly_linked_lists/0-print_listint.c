7dc0c4a378b8c2d8d48a951a74ebad5585a9e433
niyo-sean <niyibizibonaventure33@gmail.com>
2023-04-03 03:46:14 -0700

    0-task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.>
+
+/**
+ * print_listint - print all the elements of a listint_t
+ *
+ * @h: head of link list node
+ * Return: the number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	/*while loop to set condition */
+	while (h != NULL)
+	{
+		printf("\n %d", h->n);
+		h = h - > next;
+		count++;
+	}
+	return (count);
+}
+/* a NIYIBIZI Bonaventure codes */
