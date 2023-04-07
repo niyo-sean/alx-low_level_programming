bfa4d33a13cb304916d34b7ec5ef00711447a5ef
niyo-sean <niyibizibonaventure33@gmail.com>
2023-04-07 04:19:17 -0700

    2.task and its main and executable files

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
  * get_bit - returns the value of a bit at a given index.
  * @index:is the index, starting from 0 of the bit you want to get
  * @n: is the bit
- * Returns: the value of the bit at index index or -1 if an error occured
+ * Return: the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
