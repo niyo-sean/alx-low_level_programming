bce172c5d73e391e453326bca51b5c10488bf820
whapoproject <whapoproject@gmail.com>
2023-08-31 20:16:50 +0200

    Tast 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string containing binary digits.
+ *
+ * Return: The converted number, or 0 if invalid characters found or b is NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0); /* Invalid character found */
+		result = result * 2 + (*b - '0');
+		b++;
+	}
+
+	return (result);
+}
+
